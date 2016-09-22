3c6b0f53bebf0f478c8d9e6d5d55430682e97fc0
rtrevino821 <rtrevino821@gmail.com>
2016-09-22 19:25:59 -0400

    Updated Code

--- app/src/main/java/com/example/hertzfastlane/UserActivity.java
+++ app/src/main/java/com/example/hertzfastlane/UserActivity.java
@@ -1,7 +1,8 @@
 package com.example.hertzfastlane;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -14,6 +15,11 @@ public class UserActivity extends AppCompatActivity {
 
         final EditText etName = (EditText) findViewById(R.id.etName);
         final TextView welcomeMessage = (TextView) findViewById(R.id.tvWelcome);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+
+        etName.setText(name);
 
     }
 }
