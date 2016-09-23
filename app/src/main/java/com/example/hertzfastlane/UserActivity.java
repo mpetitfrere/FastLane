2fed214109c90349f942dde784c9a0c20d1774a4
rtrevino821 <rtrevino821@gmail.com>
2016-09-22 22:17:25 -0400

    QR and Edited Home Screen xml

--- app/src/main/java/com/example/hertzfastlane/UserActivity.java
+++ app/src/main/java/com/example/hertzfastlane/UserActivity.java
@@ -3,8 +3,9 @@ package com.example.hertzfastlane;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class UserActivity extends AppCompatActivity {
 
@@ -14,12 +15,25 @@ public class UserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user);
 
         final EditText etName = (EditText) findViewById(R.id.etName);
-        final TextView welcomeMessage = (TextView) findViewById(R.id.tvWelcome);
+        final Button bMyReservation = (Button) findViewById(R.id.bMyReservation);
+        final Button bScanner = (Button) findViewById(R.id.bScanVehicle);
+        final Button bMap = (Button) findViewById(R.id.bMap);
+        final Button bHelp = (Button) findViewById(R.id.bHelp);
 
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
 
         etName.setText(name);
+
+        bScanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(UserActivity.this, QrScanner.class);
+                UserActivity.this.startActivity(registerIntent);
+            }
+        });
+
+
 
     }
 }
