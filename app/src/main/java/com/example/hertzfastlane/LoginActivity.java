cb89b3d4b4ec869db944e780157842fcc504e77e
mpetitfrere <rentenkz@gmail.com>
2016-09-22 22:25:33 -0400

    added firebasr

--- app/src/main/java/com/example/hertzfastlane/LoginActivity.java
+++ app/src/main/java/com/example/hertzfastlane/LoginActivity.java
@@ -3,29 +3,60 @@ package com.example.hertzfastlane;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    private String username;
+    private String password;
+    private DatabaseReference mDatabase;
+    private boolean exist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         final EditText etUsername = (EditText) findViewById(R.id.etUserName);
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
         final Button bLogin = (Button) findViewById(R.id.bLogin);
         final Button bScanner = (Button) findViewById(R.id.bScanner);
         final TextView registerLink = (TextView) findViewById(R.id.tvRegister);
 
+
         registerLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
                 LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username = etUsername.getText().toString();
+                password = etPassword.getText().toString();
+                firebase();
+                if(exist)
+                {
+                    Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                    LoginActivity.this.startActivity(registerIntent);
+                }
+
             }
         });
 
@@ -37,5 +68,36 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void firebase()
+    {
+        mDatabase.child("Users").child(username).addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Get user value
+                        User user = dataSnapshot.getValue(User.class);
+
+                        // [START_EXCLUDE]
+                        if (user.getName() == username)
+                        {
+                            // User is null, error out
+                            Log.e(TAG, "User " + username + " is unexpectedly null");
+                            Toast.makeText(LoginActivity.this,
+                                    "Error: could not fetch user.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        finish();
+                        // [END_EXCLUDE]
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.w(TAG, "getUser:onCancelled", databaseError.toException());
+                        // [START_EXCLUDE]
+                        // [END_EXCLUDE]
+                    }
+                });
     }
 }
