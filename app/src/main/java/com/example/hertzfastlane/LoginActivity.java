b30f6419228cd06e6f56883020e521b3cc675f4c
Derek Pike <dapike7979@eagle.fgcu.edu>
2016-09-30 09:59:21 -0400

    Login Activity Derek Pike

--- app/src/main/java/com/example/hertzfastlane/LoginActivity.java
+++ app/src/main/java/com/example/hertzfastlane/LoginActivity.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.google.firebase.database.DatabaseReference;
 
 public class LoginActivity extends AppCompatActivity {
@@ -16,7 +20,9 @@ public class LoginActivity extends AppCompatActivity {
     String password;
     boolean exist;
     private DatabaseReference mDatabase;
-
+    private DynamoDBMapper mapperMembers;
+    private Member member;
+    private String resultString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +42,43 @@ public class LoginActivity extends AppCompatActivity {
                 username = etUsername.getText().toString();
                 password = etPassword.getText().toString();
 
+
+                //Amazon Web Services Connection
+                //Credentials for identity pools for Table Cars and members - AWS
+                // Initialize the Amazon Cognito credentials provider for members Table
+                CognitoCachingCredentialsProvider credentialsProviderMembers = new CognitoCachingCredentialsProvider(
+                        getApplicationContext(),
+                        "us-east-1:d203cc02-4b6f-475d-84b1-93687e673058", // Identity Pool ID
+                        Regions.US_EAST_1 // Region
+                );
+                AmazonDynamoDBClient ddbClientMembers = new AmazonDynamoDBClient(credentialsProviderMembers);
+                mapperMembers = new DynamoDBMapper(ddbClientMembers);
+                
+
+                Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        member = mapperMembers.load(Member.class, username);
+                        if(member == null){
+                            System.out.println("User does not exist");
+                        }else{
+                            if(password.equals(member.getPassword())){
+                                System.out.println("SUCCESS!");
+                            }else{
+                                System.out.println("Failure");
+                            }
+                        }
+
+                    }
+                };
+                Thread thread = new Thread(runnable);
+                thread.start();
+
+                try{
+                    thread.join();
+                }catch(Exception e){
+                    return;
+                }
 //firebase();
 //if(username.toString() == "john")
 //{
--- app/src/main/java/com/example/hertzfastlane/Member.java
+++ app/src/main/java/com/example/hertzfastlane/Member.java
@@ -52,6 +52,27 @@ public class Member {
     private int id;
     private String first_name;
     private String last_name;
+
+    @DynamoDBAttribute(attributeName = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @DynamoDBAttribute(attributeName = "username")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String username;
+    private String password;
     private String  reservationVin;
 
 
